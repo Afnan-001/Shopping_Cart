ff0b54f6a38865f16c0d127bc836f094192a5349
Afnan Sayyad <afnansayyad413@gmail.com>
2025-03-25 19:47:10 +0530

    Linked Home Screen and Cart Screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,20 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:shopping_cart/views/cart_screen.dart';
+import '../providers/product_provider.dart';
+import '../widgets/product_card.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels == 
+        _scrollController.position.maxScrollExtent) {
+      ref.read(productProvider.notifier).loadProducts();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final productState = ref.watch(productProvider);
+
     return Scaffold(
-      body: Text("Hello"),
+      appBar: AppBar(
+        title: const Text('Products'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CartScreen()),
+            ),
+          ),
+        ],
+      ),
+      body: productState.when(
+        data: (products) => ListView.builder(
+          controller: _scrollController,
+          itemCount: products.length + 1,
+          itemBuilder: (context, index) {
+            if (index < products.length) {
+              return ProductCard(product: products[index]);
+            }
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 32.0),
+              child: Center(child: CircularProgressIndicator()),
+            );
+          },
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(child: Text('Error: $error')),
+      ),
     );
   }
 }
